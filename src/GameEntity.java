68b640425b9ae5ff4b8fc04bd9653b2d1e2de78f
Arun <helloarun0110@gmail.com>
2025-05-10 00:26:55 +0600

    structure design for the game

---
+++ src/GameEntity.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameEntity {
+
+    public GameEntity(){
+        
+    }
+    
+}
---
+++ src/GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    private final int titleSize = 32;
+    private final int rows = 21;
+    private final int cols = 19;
+
+    public GameFrame(){
+        setTitle("Pac-Man");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        
+    }
+    
+}
