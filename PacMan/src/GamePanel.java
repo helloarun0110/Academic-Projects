177788eb342f799e1b244081c4eb2d6017754e1c
Arun <helloarun0110@gmail.com>
2025-05-10 17:54:25 +0600

    implement GamePanel to manage game rendering, updates, and kyboard input

--- PacMan/src/GamePanel.java
+++ PacMan/src/GamePanel.java
@@ -1,9 +1,45 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class GamePanel {
-    public GamePanel(){
-        
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+    private final GameBoard board;
+    private final Timer timer;
+
+    public GamePanel(int rows, int cols, int tileSize){
+        setPreferredSize(new Dimension(cols * tileSize, rows * tileSize));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        addKeyListener(this);
+
+
+        board = new GameBoard(rows, cols, tileSize);
+        timer = new Timer(50,this);
+        timer.start();
+
     }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        board.draw(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        board.update();
+        repaint();
+        if (board.isGameOver()) timer.stop();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        board.handleKeyPress(e);
+    }
+
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
 }
